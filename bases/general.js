ed51ee1c70fb208958910448023050bdc20a3545
Sabrina Godoy <sabrygodoy1@gmail.com>
2025-12-17 21:34:24 -0300

    variables

---
+++ bases/general.js
@@ -0,0 +1,10 @@
+// Declarar una variable
+let edad;
+let nombre;
+
+// Inicializar una variable
+edad = 28;
+nombre = "Sabrina";
+
+// Declarar e inicializar una variable
+let nacionalidad = "Argentina";
