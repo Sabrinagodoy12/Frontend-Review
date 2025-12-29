44ece7518c27587be6951ed3c120c7dbba1f77e3
Sabrina Godoy <sabrygodoy1@gmail.com>
2025-12-29 16:43:03 -0300

    tipo de datos + distintas variables

---
+++ bases/apuntes.js
@@ -0,0 +1,13 @@
+// Declarar variable
+let edad;
+let nombre;
+
+// Inicializar variable
+edad = 28;
+nombre = "Sabrina";
+
+// Declarar e inicializar variable
+let nacionalidad = "Argentina";
+
+// Declarar variable Constante
+const idioma = "Español";
---
+++ bases/pruebas.js
@@ -0,0 +1,7 @@
+let nombre = "Sabrina";
+let edad = 28;
+let presenteONo = true;
+
+console.log("Nombre: " + nombre + " - Tipo: " + typeof nombre); // Typeof: para ver el tipo de dato
+console.log("Edad: " + edad + " - Tipo: " + typeof edad);
+console.log("Pesente: " + presenteONo + " - Tipo: " + typeof presenteONo);
